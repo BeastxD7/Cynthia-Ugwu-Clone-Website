c8ff7f185cc6f81bac7fc8edde25375e555b2b9a
Shashank Devadiga <beast.steam002@gmail.com>
2023-11-21 19:38:35 +0530

    added minicirle mouse follower js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function circlemousefollower() {
+    window.addEventListener("mousemove",function(dets){
+        document.querySelector("#minicircle").style.transform  =`translate(${dets.clientX}px, ${dets.clientY}px)`
+    })
+}
+circlemousefollower();
